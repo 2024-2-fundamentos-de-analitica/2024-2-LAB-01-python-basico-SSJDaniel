013a38fcd4342f2f251d9ab95d299b5f0c1ff1e8
Daniel Sanchez <dasanchezvi@unal.edu.co>
2024-11-28 17:13:55 -0500

    Done

--- homework/pregunta_12.py
+++ homework/pregunta_12.py
@@ -5,8 +5,6 @@ solo puede utilizar las funciones y librerias basicas de python. No puede
 utilizar pandas, numpy o scipy.
 """
 
-
-def pregunta_12():
     """
     Genere un diccionario que contengan como clave la columna 1 y como valor
     la suma de los valores de la columna 5 sobre todo el archivo.
